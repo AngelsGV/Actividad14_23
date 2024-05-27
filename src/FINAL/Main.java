d109647d0cba03880896d70b9edcfb459f3ab707
AngelsGV <angels.not99@gmail.com>
2024-05-27 20:20:42 +0200

    Initial commit

--- src/FINAL/Main.java
+++ src/FINAL/Main.java
@@ -9,29 +9,27 @@ package FINAL;
 public class Main {
     public static void main(String[] args) {
         OficinaDAO oficinaDAO = new OficinaDAO();
-    //Oficina 11. Sevilla. 100 	10000.00
-
-        int noOficina = 11;
+    //Oficina 11. Sevilla. 100 	10000.0
+        int nOficina = 11;
 
         // Mostrar datos antes de la modificación
-        Oficina oficinaAntes = oficinaDAO.selectOficina(noOficina);
+        Oficina oficinaAntes = oficinaDAO.selectOficina(nOficina);
         System.out.println("Antes de la modificación: " + oficinaAntes);
 
-        if (oficinaAntes != null) {
+        if (oficinaAntes != null) { //si la oficina no está vacía
 
-            // Modificar la ciudad y aumentar las ventas
-            oficinaAntes.setCiudad("Vinaròs");
-            oficinaAntes.setVentas(oficinaAntes.getVentas() - 1000);
-
+            // Modificar la ciudad y aumentar las ventas. Pon lo que te interese
+            oficinaAntes.setCiudad("Castellón");
+            oficinaAntes.setVentas(oficinaAntes.getVentas() + 1000);
             if (oficinaDAO.updateOficina(oficinaAntes)) {
                 // Mostrar datos después de la modificación
-                Oficina oficinaDespues = oficinaDAO.selectOficina(noOficina);
+                Oficina oficinaDespues = oficinaDAO.selectOficina(nOficina);
                 System.out.println("Después de la modificación: " + oficinaDespues);
             } else {
-                System.out.println("No se pudo actualizar la oficina.");
+                System.out.println("No se pudo hace el cambio.");
             }
         } else {
-            System.out.println("La oficina con el número " + noOficina + " no existe.");
+            System.out.println("La oficina con el número " + nOficina + " no existe.");//Si está vacía
         }
     }
 }
